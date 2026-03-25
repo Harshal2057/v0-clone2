246b76fe96f074aff163ecb262c54696877f8a65
Harshal2057 <harshcbhandarkar@gmail.com>
2026-03-26 04:06:50 +0530

    Home Page

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,14 +1,36 @@
-import Navbar from "../../modules/home/components/navbar";
-import { Button } from "../../components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import ProjectForm from "../../modules/home/components/project-form";
 import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Navbar />
-      <Button>Test</Button>
-      <UserButton />
+    <div className="flex justify-center items-center w-full px-4 py-8">
+      <div className="max-w-5xl w-full">
+        <section className="space-y-8 flex flex-col items-center">
+          <div className="flex flex-col items-center">
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              width={100}
+              height={100}
+              className="hidden md:block invert dark:invert-0"
+            />
+          </div>
+          <h1 className="text-2xl md:text-5xl font-bold text-center">
+            Build Something with 💓
+          </h1>
+
+          <p className="text-lg md:text-xl text-muted-foreground text-center">
+            Create apps and websites by chatting with AI
+          </p>
+
+          <div className="max-w-3xl w-full">
+              <ProjectForm />
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "../components/theme-provider";
+import { Toaster } from "../components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,20 +21,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-        >
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <ClerkProvider>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-          ></ThemeProvider>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+          >
+            <Toaster />
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
